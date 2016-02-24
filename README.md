9aa6d8304f345c19f78c3239769bc49c58642de6
jyoko <jyoko@users.noreply.github.com>
2016-02-23 23:04:03 -0500

    Initial Release
    * Bugfix following to not toggle in textareas (and test update)

---
+++ README.md
@@ -0,0 +1,9 @@
+# Reddit-Sidebar
+
+`script.js` is both GreaseMonkey & TamperMonkey compatible.
+
+I like having multiple windows and the right sidebar gets annoying (takes up ~33% of screen real estate), so I fixed that. Didn't find any reasonable premade solutions, but [/u/GameFreak4321 in this thread](https://www.reddit.com/r/Enhancement/comments/swp8s/feature_request_a_button_to_hide_the_sidebar) had a pretty nice solution.
+
+Basically, instead of completely hiding the sidebar (and breaking some subreddits), we move it off the side of the screen. You can easily scroll right to see it and any repositioned elements will hopefully still appear correctly.
+
+I added `q` as a toggle key, as it doesn't interfere with any of my other shortcuts.
---
+++ script.js
@@ -0,0 +1,32 @@
+// ==UserScript==
+// @name         Reddit Sidebar Offscreen
+// @namespace    https://jyoko.github.io/
+// @version      1.0
+// @description  Moves Reddit right sidebar offscreen, uses "q" to toggle
+// @author       jyoko
+// @downloadURL  https://raw.githubusercontent.com/jyoko/reddit-sidebar/master/script.js
+// @require 
+// @includes     *://*.reddit.com
+// @grant        none
+// ==/UserScript==
+/* jshint -W097 */
+'use strict';
+
+if (window.hasOwnProperty('$')) {
+  window.addEventListener('keyup', function(e) {
+    if (e.keyCode === 81) { // q
+      toggler($('.side'));
+    }
+  });
+  toggler($('.side'));
+}
+// Full credit to /u/GameFreak4321 here, excellent idea
+// https://www.reddit.com/r/Enhancement/comments/swp8s/feature_request_a_button_to_hide_the_sidebar
+function toggler(side) {
+  if (side.hasClass('RESSidebarHide')){
+    side.css('margin-right', '0px').removeClass('RESSidebarHide');
+  } else {
+    side.css('margin-right','-'+side.width()+'px').addClass('RESSidebarHide');
+  }            
+}
+
